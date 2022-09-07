5ccacf306b0426ac02c36f47056d6a4e1ba7b9c2
Moechtar Wira <moechtarwira252@gmail.com>
2022-09-07 13:43:31 +0700

    parameter changes

--- soal2.js
+++ soal2.js
@@ -1,39 +1,41 @@
 class Animal {
-    constructor (nama, spesies, habitat){
+    constructor (nama, umur, kaki, spesies, status){
         this.nama = nama
+        this.umur = umur
+        this.kaki = kaki
         this.spesies = spesies
-        this.habitat = habitat
+        this.status = status
     }
-    introduction (){
-        return `Halo, saya ${this.nama}, saya adalah seekor ${this.spesies}, saya tinggal di ${this.habitat}`
-    }
+    // introduction (){
+    //     console.log(`Halo, nama saya ${this.nama}, saya adalah seekor ${this.spesies}, saya berumur ${this.umur}, saya ${this.status}.`); 
+    // }
 }
 
 class Hiu extends Animal {
-    constructor (nama, spesies, habitat){
-        super (nama, spesies, habitat)
+    constructor (nama, umur, kaki, spesies, status){
+        super (nama, umur, kaki, spesies, status)
     }
     introduction (){
-        console.log(`Halo, nama saya ${this.nama}, saya adalah seekor ${this.spesies}, saya tinggal di ${this.habitat}`); 
+        console.log(`Halo, nama saya ${this.nama}, saya adalah seekor ${this.spesies}, saya berumur ${this.umur}, saya ${this.status}.`); 
     }
 }
 
 class Kucing extends Animal {
-    constructor (nama, spesies, habitat){
-        super (nama, spesies, habitat)
+    constructor (nama, umur, kaki, spesies, status){
+        super (nama, umur, kaki, spesies, status)
     }
     introduction (){
-        console.log(`Halo, nama saya ${this.nama}, saya adalah seekor ${this.spesies}, saya tinggal di ${this.habitat}`); 
+        console.log(`Halo, nama saya ${this.nama}, saya adalah seekor ${this.spesies}, saya berumur ${this.umur}, saya ${this.status}.`); 
     }
 }
 
 class Anjing extends Animal {
-    constructor (nama, spesies, habitat, pemilik){
-        super (nama, spesies, habitat)
+    constructor (nama, umur, kaki, spesies, status, pemilik){
+        super (nama, umur, kaki, spesies, status)
         this.pemilik = pemilik
     }
     introduction (){
-        console.log(`Halo, nama saya ${this.nama}, saya adalah seekor ${this.spesies}, saya tinggal di ${this.habitat}`); 
+        console.log(`Halo, nama saya ${this.nama}, saya adalah seekor ${this.spesies}, saya berumur ${this.umur}, saya ${this.status}.`); 
     }
     greetMaster(){
         console.log(`Halo ${this.pemilik}`);
@@ -42,16 +44,15 @@ class Anjing extends Animal {
 
 }
 
-const hiu = new Hiu('Hiu', 'Ikan', 'Lautan');
+const hiu = new Hiu('Hiu', 10, 0, 'Ikan', 'Berenang');
 hiu.introduction()
+console.info(hiu);
 
-const kucing = new Kucing('Kucing', 'Mamalia', 'Daratan');
+const kucing = new Kucing('Kucing', 6, 4, 'Mamalia', 'Berjalan');
 kucing.introduction()
+console.info(kucing);
 
-const anjing = new Anjing('Anjing', 'Mamalia', 'Daratan', 'Wira');
+const anjing = new Anjing('Anjing', 7, 4, 'Mamalia', 'Berjalan', 'Wira');
 anjing.introduction()
 anjing.greetMaster()
-
-// console.info(hiu);
-// console.info(kucing);
-// console.info(anjing);
+console.info(anjing);
